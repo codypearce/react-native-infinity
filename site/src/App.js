3b2d478d09fe689227c7ea9019eaf64590d3832a
codypearce <codyapearce@gmail.com>
2019-08-17 17:22:00 -0700

    Init site

---
+++ site/src/App.js
@@ -0,0 +1,14 @@
+// component.jsx
+import React from "react";
+import ReactDOM from "react-dom";
+import { Text, View, StyleSheet } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>React Native Infinity</Text>
+      </View>
+    );
+  }
+}
---
+++ site/webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules\/(?!(material-bread|react-native-vector-icons)\/).*/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./dist/index.html",
+      filename: "./index.html"
+    })
+  ],
+  resolve: {
+    // auto resolves any react-native import as react-native-web
+    alias: {
+      "react-native": "react-native-web"
+    },
+    extensions: [".web.js", ".js"]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: "./",
+    hot: true,
+    port: 8008
+  }
+};
