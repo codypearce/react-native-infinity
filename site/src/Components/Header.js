e74f88d193baa6439f3184559c9972c6f0783c67
codypearce <codyapearce@gmail.com>
2019-08-17 18:06:58 -0700

    Add link to title

--- site/src/Components/Header.js
+++ site/src/Components/Header.js
@@ -2,15 +2,19 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { Appbar } from "material-bread";
 import HeaderLink from "./HeaderLink";
+import { Link } from "react-router-dom";
 
 export default class Header extends React.Component {
   render() {
     return (
       <Appbar
         barType={"normal"}
-        title={"React Native Infinity"}
+        title={
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            React Native Infinity
+          </Link>
+        }
         actionItems={[
-          <HeaderLink text={"Home"} to="/" />,
           <HeaderLink text={"Interactive"} to="/interactive" />,
           <HeaderLink text={"CLI"} to="/cli" />
         ]}
