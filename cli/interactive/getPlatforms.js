4f1d5772e02e91876c9708c69b101f2b2c7a7605
codypearce <codyapearce@gmail.com>
2019-09-14 21:45:27 -0700

    Handle no platforms selected

--- cli/interactive/getPlatforms.js
+++ cli/interactive/getPlatforms.js
@@ -1,7 +1,9 @@
 const { prompt } = require("enquirer");
+const chalk = require("chalk");
 const init = require("../commands/init/index");
+const newLine = require("../console/newLine");
 
-module.exports = function(name) {
+module.exports = function getPlatforms(name) {
   prompt([
     {
       type: "multiselect",
@@ -20,6 +22,16 @@ module.exports = function(name) {
     }
   ]).then(answers => {
     const platformArr = Object.values(answers.platforms);
+    if (platformArr && platformArr.length < 1) {
+      newLine(2);
+      console.log(
+        chalk.red.bold("You must choose at least one platform to support")
+      );
+
+      newLine(2);
+      getPlatforms(name);
+      return;
+    }
     const platformString = platformArr.join("");
     init(name, { starter: platformString });
   });
