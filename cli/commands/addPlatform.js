76c195968db540ada038f1465ebd4880948a3e3f
codypearce <codyapearce@gmail.com>
2019-09-15 16:37:25 -0700

    add ending text to add-platform

--- cli/commands/addPlatform.js
+++ cli/commands/addPlatform.js
@@ -1,10 +1,10 @@
 const fs = require("fs-extra");
 const path = require("path");
 const merge = require("merge-package-json");
+const chalk = require("chalk");
 
 module.exports = function addPlatform(platform) {
   const { starter, platforms } = findStarter(platform);
-  console.log(starter, platforms);
 
   if (starter == "mobile") {
     console.log(chalk.cyan("Setting up iOS"));
@@ -39,6 +39,11 @@ module.exports = function addPlatform(platform) {
         "ERROR"
       )}: Cannnot find your project's package.json, please go to the root of your poject and try again.`
     );
+
+    newLine(1);
+    // Remove Created Folders
+
+    return;
   }
   let newPackage = currentPackageJSON;
 
@@ -52,9 +57,42 @@ module.exports = function addPlatform(platform) {
   newPackage = merge(newPackage, platformJSON);
 
   const copyPackageJson = fs.writeFileSync(`./package.json`, newPackage);
+
+  newLine(1);
+
+  packagename();
+  horizontalLine(1);
+
+  console.log(chalk.cyan("Get started:"));
+  console.log(chalk.cyan("1."), chalk.blue.bold("npm i"));
+
+  if (starter == "mobile") {
+    console.log(
+      chalk.cyan("2."),
+      chalk.blue.bold(`npx react-native-rename ${name}`)
+    );
+  }
+  newLine(1);
+  console.log(
+    chalk.cyan(
+      "Finally run the command for the platform(s) you'd like to render to:"
+    )
+  );
+
+  platformCommand(starter);
+
+  newLine(1);
+  horizontalLine(1);
+  console.log(
+    "Please visit the documentation for more information: https://github.com/codypearce/react-native-infinity"
+  );
+  horizontalLine(1);
+  newLine(1);
 };
 
 // Avoid Circular Dependency
 const findStarter = require("../lib/findStarter");
-const chalk = require("chalk");
 const newLine = require("../console/newLine");
+const horizontalLine = require("../console/horizontalLine");
+const packagename = require("../console/packageName");
+const platformCommand = require("../console/platformCommand");
