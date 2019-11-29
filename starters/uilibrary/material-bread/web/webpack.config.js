f72c9f7fa05e42954c847df1a1c4658292457c70
codypearce <codyapearce@gmail.com>
2019-11-29 03:07:48 -0800

    Add bagel transforms to mb

--- starters/uilibrary/material-bread/web/webpack.config.js
+++ starters/uilibrary/material-bread/web/webpack.config.js
@@ -13,6 +13,14 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+              '@babel/plugin-transform-flow-strip-types',
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-proposal-export-default-from',
+              '@babel/plugin-transform-runtime',
+              '@babel/plugin-transform-regenerator',
+            ],
           },
         },
       },
